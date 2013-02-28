5648b552c8ed32065b84c16862bd56b5f6fc6b70
John Robson <John.Robson@USP.br>
2013-02-27 19:43:18 -0500

    WApplication::instance()->require

--- Localized/test.cpp
+++ Localized/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Wt/WApplication>
 #include <Wt/WContainerWidget>
+#include <Wt/WTemplate>
 #include <Wt/WText>
 #include <Wt/WString>
 
@@ -12,9 +13,12 @@ class WtApplication : public WApplication { public: WtApplication(const WEnviron
 WtApplication::WtApplication(const WEnvironment& env) : WApplication(env) {
   messageResourceBundle().use(appRoot() + "Localized/main");
 
+  WApplication::instance()->require("http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
+
+  //WTemplate* tp = new WTemplate(WString::tr("index"), root());
+
   WText* t = new WText(WString::tr("index"));
   t->setTextFormat(Wt::XHTMLUnsafeText);
-
   root()->addWidget(t);
 }
 
